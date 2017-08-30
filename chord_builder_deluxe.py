84c976cdb9840dbd45c895242488c0db14ec55ed
Nahiyan Khandker <nahiyan.khandker@gmail.com>
2017-08-29 22:20:45 -0500

    changed data some data representations and added an ideas for finding all semitones in a given interval

--- chord_builder_deluxe.py
+++ chord_builder_deluxe.py
@@ -6,19 +6,7 @@ from enum import Enum
 
 class Notes(Enum):
     __order__ = 'A B C D E F G'
-    A = 1;
-    B = 3;
-    C = 4;
-    D = 6;
-    E = 8;
-    F = 9;
-    G = 11;
-
-notes = list(Notes)
-
-class Flats(Enum):
-    __order__ = 'A B C D E F G'
-    A = 12;
+    A = 0;
     B = 2;
     C = 3;
     D = 5;
@@ -26,23 +14,70 @@ class Flats(Enum):
     F = 8;
     G = 10;
 
-flats = list(flats)
+orderedNotes = list(Notes)
 
-class Sharps(Enum):
-    __order__ = 'A B C D E F G'
-    A = 2;
-    B = 4;
-    C = 5;
-    D = 7;
-    E = 9;
-    F = 10;
-    G = 12;
+notes = {
+    "A*" : 11,
+    "A"  : 0,
+    "A#" : 1,
 
-sharps = list(Sharps)
+    "B*" : 1,
+    "B"  : 2,
+    "B#" : 3,
+
+    "C*" : 2,
+    "C"  : 3,
+    "C#" : 4,
+
+    "D*" : 4,
+    "D"  : 5,
+    "D#" : 6,
+
+    "E*" : 6,
+    "E"  : 7,
+    "E#" : 8,
+
+    "F*" : 7,
+    "F"  : 8,
+    "F#" : 9,
+
+    "G*" : 9,
+    "G"  : 10,
+    "G#" : 11
+}
+
+# going up full steps and half steps on semitones
+
+def fullstep(pitch):
+    return (pitch + 2)%12
+
+def halfstep(pitch):
+    return (pitch + 1)%12
+
+#interval patterns shown as arrays
+
+Major = ['f','f','h','f','f','f','h']
+
+#Given a note and interval pattern, gives you all the semitones
+
+def NotesInKey(note,KeyType):
+    result = notes.get(note)
+    results = [result]
+    print(result)
+    for x in KeyType:
+        if x == 'f':
+            result = fullstep(result)
+        elif x == 'h':
+            result = halfstep(result)
+        results.append(result)
+    return results
+
 
 def getToneFromNote():
     #notesList = list(Notes)
     for i in range(0,3):
         print(notes[i])
 
-getToneFromNote()
+var = NotesInKey("A", Major)
+
+print(var)
